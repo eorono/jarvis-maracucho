3858157574f5b85c1d07bda50d4fd2a93053a916
eorono <eorono@gmail.com>
2024-03-11 17:29:19 -0400

    Take user imput from Speech recognition

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pyttsx3
+import speech_recognition as sr
 from decouple import config
 from datetime import datetime
+from random import choice
+from utils import opening_text
 
 
 USERNAME = config('USER')
@@ -37,5 +40,33 @@ def greet_user():
     elif (hour >= 16) and (hour < 19):
         speak(f"Good Evening {USERNAME}")
     speak(f"I am {BOTNAME}. How may I assist you?")
+
+## Takes Input from User
+def take_user_input():
+    """Takes user input, recognizes it using Speech Recognition module and converts it into text"""
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening....')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language='en-in')
+        if not 'exit' in query or 'stop' in query:
+            speak(choice(opening_text))
+        else:
+            hour = datetime.now().hour
+            if hour >= 21 and hour < 6:
+                speak("Good night sir, take care!")
+            else:
+                speak('Have a good day sir!')
+            exit()
+    except Exception:
+        speak('Sorry, I could not understand. Could you please say that again?')
+        query = 'None'
+    return query
+
 
 
