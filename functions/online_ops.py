5e4e53428c7d7342bc52fb40ac3296ca173445a8
eorono <eorono@gmail.com>
2024-03-12 16:51:39 -0400

    online functions added, send a whatsapp and send a EMAIL

--- functions/online_ops.py
+++ functions/online_ops.py
@@ -23,3 +23,28 @@ def play_on_youtube(video):
 def search_on_google(query):
     kit.search(query)
 
+
+def send_whatsapp_message(number, message):
+    kit.sendwhatmsg_instantly(f"+91{number}", message)
+
+
+EMAIL = config("EMAIL")
+PASSWORD = config("PASSWORD")
+
+def send_email(receiver_address, subject, message):
+    try:
+        email = EmailMessage()
+        email['To'] = receiver_address
+        email["Subject"] = subject
+        email['From'] = EMAIL
+        email.set_content(message)
+        s = smtplib.SMTP("smtp.gmail.com", 587)
+        s.starttls()
+        s.login(EMAIL, PASSWORD)
+        s.send_message(email)
+        s.close()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
