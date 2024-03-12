0dea5405e7cab20509355c8a4f600e33ff1ae703
eorono <eorono@gmail.com>
2024-03-12 16:52:39 -0400

    online functions added, get news and weather report (need api keys to work)

--- functions/online_ops.py
+++ functions/online_ops.py
@@ -48,3 +48,28 @@ def send_email(receiver_address, subject, message):
         print(e)
         return False
 
+
+NEWS_API_KEY = config("NEWS_API_KEY")
+
+
+def get_latest_news():
+    news_headlines = []
+    res = requests.get(
+        f"https://newsapi.org/v2/top-headlines?country=in&apiKey={NEWS_API_KEY}&category=general").json()
+    articles = res["articles"]
+    for article in articles:
+        news_headlines.append(article["title"])
+    return news_headlines[:5]
+
+
+OPENWEATHER_APP_ID = config("OPENWEATHER_APP_ID")
+
+
+def get_weather_report(city):
+    res = requests.get(
+        f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_APP_ID}&units=metric").json()
+    weather = res["weather"][0]["main"]
+    temperature = res["main"]["temp"]
+    feels_like = res["main"]["feels_like"]
+    return weather, f"{temperature}℃", f"{feels_like}℃"
+
