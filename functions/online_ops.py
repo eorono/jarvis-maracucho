dc6123c97e516c477418c7796f29e9bbc305cf47
eorono <eorono@gmail.com>
2024-03-12 16:50:54 -0400

    online functions added, play on Youtube and SearchOnGoogle

--- functions/online_ops.py
+++ functions/online_ops.py
@@ -14,3 +14,12 @@ def find_my_ip():
 def search_on_wikipedia(query):
     results = wikipedia.summary(query, sentences=2)
     return results
+
+
+def play_on_youtube(video):
+    kit.playonyt(video)
+
+
+def search_on_google(query):
+    kit.search(query)
+
